787d4beef6ea3082e80d08a01098524556cea229
rxh5296 <rxh5296@OZWDLND132-217.win.psu.edu>
2017-10-09 08:48:45 -0400

    Added hello to line 16 of the WheelOfFortune class.

--- src/wheeloffortune/WheelOfFortune.java
+++ src/wheeloffortune/WheelOfFortune.java
@@ -13,7 +13,8 @@ public class WheelOfFortune {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    
+      System.out.println("Hello!");
+       
   }
   
 }
